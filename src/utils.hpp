431a389b0e9b0f8c91e2154f222a53f8f4ae63a0
Ryo Okubo <syucream1031@gmail.com>
2016-07-12 00:56:57 +0900

    Replace ugly macro with SFINAE

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,20 +1,46 @@
+#include <type_traits>
+
 namespace ts_mruby {
 
 namespace utils {
 
-template<typename T, typename... Args>
+
+namespace {
+
+extern void *enabler;
+
+/*
+ * meta functions check 'type T' has mock_type or not
+ */
+struct has_mock_impl {
+  template<typename T>
+  static std::true_type check(typename T::mock_type*);
+
+  template<typename T>
+  static std::false_type check(...);
+};
+template<typename T>
+class has_mock : public decltype(has_mock_impl::check<T>(nullptr)) {};
+
+} // anonymous namespace
+
+
+// Allocate non-mocked object
+template<typename T, typename... Args, typename std::enable_if<!has_mock<T>::value>::type*& = enabler>
 T* mockable_ptr(Args... args) {
-#ifndef MOCKING
   return new T(args...);
-#else
-  // FIXME it needs mutual exclusion and calling delete ...
+}
+
+// Allocate or get mocked object
+// FIXME it needs mutual exclusion and calling delete ...
+template<typename T, typename... Args, typename std::enable_if<has_mock<T>::value>::type*& = enabler>
+T* mockable_ptr(Args... args) {
   using T_MOCK = typename T::mock_type;
   static T_MOCK* ptr = nullptr;
   if (!ptr) {
     ptr = new T_MOCK(args...);
   }
   return ptr;
-#endif
 }
 
 } // utils namespace
