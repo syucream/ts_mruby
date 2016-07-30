2a46156f05ab996ac8b9264bb59052877751d838
Ryo Okubo <syucream1031@gmail.com>
2016-07-31 01:56:11 +0900

    Simplify generating config files

--- spec/ats_config/ats_echo.rb
+++ spec/ats_config/ats_echo.rb
@@ -1,16 +1,16 @@
 require 'factory_girl'
 
 # load fixtures
-FactoryGirl.definition_file_paths = %w(./factories/)
+FactoryGirl.definition_file_paths = %w(../factories/)
 FactoryGirl.find_definitions
 include FactoryGirl::Syntax::Methods
 
 # variables
-rputs = build(:rputs)
+echo = build(:echo)
 
 # generate template
 template = <<EOS
-ATS::rputs "#{rputs.body}"
+ATS::echo "#{echo.body}"
 EOS
 
 puts template
