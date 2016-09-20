3ee9360594280cc20627abfe2909ceceb69b7adc
Ryo Okubo <syucream1031@gmail.com>
2016-09-20 21:26:03 +0900

    Fix #76 Support Request#hostname

---
+++ spec/ats_config/ats_request.rb
@@ -0,0 +1,9 @@
+require '../support/factory_girl_helper'
+include FactoryGirlHelper
+
+# generate template
+puts <<EOS
+template = "#{get(:request).template}"
+req = ATS::Request.new
+ATS::echo template % {hostname: req.hostname}
+EOS
