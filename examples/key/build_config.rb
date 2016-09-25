a3d36e97e04f874c46dab75767b167e31d5ba27a
Ryo Okubo <syucream1031@gmail.com>
2016-09-25 15:56:14 +0900

    wip

--- examples/key/build_config.rb
+++ examples/key/build_config.rb
@@ -2,6 +2,7 @@ MRuby::Build.new do |conf|
   toolchain :gcc
 
   conf.gembox 'default'
+  conf.gem :mgem => 'mruby-onig-regexp'
   # conf.gem :mgem => 'mruby-redis'
   conf.gem :github => 'matsumoto-r/mruby-redis'
 end
--- examples/key/key.rb
+++ examples/key/key.rb
@@ -13,41 +13,25 @@ def calculate_key(key_value, headers)
     raise FailingParameterProcessing if fname_index.nil?
 
     # 5-3)
-    field_name = key_item[0...fname_index]
+    field_name = key_item[0...fname_index].gsub(/^\s*(.+)\s*$/){$1}
 
     # 5-4)
     field_value = headers[field_name]
 
     # 5-5)
-    parameters = key_item[fname_index+1..key_item.length]
+    parameters = key_item[fname_index+1..key_item.length].gsub(/^\s*(.+)\s*$/){$1}
 
     # 5-6)
-    pi = 0; ci = 0
-    param_list = []
-    while ci = parameters.index(';', ci); !ci.nil?
-      # excepting ";" characters within quoted strings
-      if ci < parameters.length && parameters[ci-1] == '\"' && parameters[ci+1] = '\"'
-        next
-      end
-      param_list.push(parameters[pi..ci])
-      pi = ci + 1
-    end
-    param_list.push(parameters[pi..parameters.length-1])
+    param_list = parameters.split(/(?!");(?!")/)
 
     # 5-7)
     param_list.map do |parameter|
-      pair = parameter.split('=')
+      pair = parameter.gsub(/^\s*(.+)\s*$/){$1}.split('=')
       next if pair.length != 2
-      param_name = pair[0].gsub(' ', '').downcase
-      param_value = pair[1].gsub(' ', '')
+      param_name = pair[0].downcase
+      param_value = pair[1].gsub(/^\"(.+)\"$/){ '\\' + $1 }
 
-      # If the first and last characters of "param_value" are both DQUOTE:
-      if param_value[0] == '"' && param_value[param_value.length-1] == '"'
-        param_value[0] = '\\'
-        param_value.chop!
-      end
-
-      calculate_each_(param_name, param_value, field_value)
+      calculate_keyparam_(param_name, param_value, field_value)
     end.join('')
   end
   
@@ -55,34 +39,76 @@ def calculate_key(key_value, headers)
   seckey.join("\000")
 end
 
+#
 # 2.3. Key Parameters
-def calculate_each_(param_name, param_value, field_value)
-  key_tmp = ''
-
-  # TODO 2.3.1. div
-  # TODO 2.3.2. partition
-  # TODO 2.3.3. match
-
-  # 2.3.4. substr
-  if param_name
-    if !field_value.empty?
-      key_tmp = field_value.include?(param_value) ? '1' : '0'
+#
+# return a part of secondary cache key from given pair of parameter and field_value
+#
+def calculate_keyparam_(param_name, param_value, field_value)
+  case param_name
+  when 'div'
+    return 'none' if field_value.empty?
+    raise FailingParameterProcessing if param_value == '0'
+    if field_value =~ (/^\s*(\d+)\s*,?.*$/)
+      return ($1.to_i / param_value.to_i).to_i.to_s
     else
-      key_tmp = 'none'
+      raise FailingParameterProcessing
     end
+
+  when 'partition'
+    return 'none' if field_value.empty?
+    if field_value =~ (/^\s*(\d+)(\.\d+)?\s*,?.*$/)
+      header_value = $1 + ($2.nil? ? '' : $2)
+      segment_id = 0
+      param_value.split(':').each do |segment_value|
+        next if header_value.to_f < segment_value.to_f
+        segment_id += 1
+      end
+      return segment_id.to_s
+    else
+      raise FailingParameterProcessing
+    end
+
+  when 'match'
+    return 'none' if field_value.empty?
+    exist = field_value.split(',').any? do |header_item|
+      header_item.gsub(/^\s(.+)\s$/){$1} === param_value
+    end
+    return exist ? "1" : "0"
+
+  when 'substr'
+    return 'none' if field_value.empty?
+    exist = field_value.split(',').any? do |header_item|
+      header_item.gsub(/^\s(.+)\s$/){$1}.include?(param_value)
+    end
+    return exist ? "1" : "0"
+
+  when 'param'
+    header_list = []
+    field_value.split(',').each do |header_item_tmp1|
+      header_item_tmp1.split(';').each do |header_item_tmp2|
+        header_list.push(header_item_tmp2.gsub(/^\s(.+)\s$/){$1})
+      end
+    end
+    header_list.each do |header_item|
+      if header_item =~ /^(.+)=(.+)$/
+        return $2 if param_value === $1
+      else
+        next
+      end
+    end
+    return ''
+
+  else
+    raise FailingParameterProcessing
   end
-
-  # TODO 2.3.5. param
-
-  key_tmp
 end
 
 # Get request headers converted to lower case
 def get_headers
   hin = ATS::Headers_in.new
   hin.all.reduce({}) do |memo, pair| 
-    memo[pair.first.downcase] = pair[1]
-    memo
+    memo[pair.first.downcase] = pair[1]; memo
   end
 end
 
