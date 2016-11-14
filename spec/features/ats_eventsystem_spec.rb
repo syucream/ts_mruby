b0e8fc7483f6c3dbb28e2005f1395a3298b104b9
Ryo Okubo <syucream1031@gmail.com>
2016-11-14 23:12:37 +0900

    Fix a typo

--- spec/features/ats_eventsystem_spec.rb
+++ spec/features/ats_eventsystem_spec.rb
@@ -1,6 +1,6 @@
 require './spec_helper'
 
-describe 'ATS::Filter class', :js => false do
+describe 'ATS::EventSystem class', :js => false do
   let (:register) { get(:eventsystem_register) }
 
   describe 'register' do
@@ -11,6 +11,7 @@ describe 'ATS::Filter class', :js => false do
       send_request = register.send_request_hdr
       read_response = register.read_response_hdr
       send_response = register.send_response_hdr
+
       expect(hdr[send_request['key']]).to eq send_request['value']
       expect(hdr[read_response['key']]).to eq read_response['value']
       expect(hdr[send_response['key']]).to eq send_response['value']
